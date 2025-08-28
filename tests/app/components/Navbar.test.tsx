ace0fc0f8051bac472f2144b1d706048b04b262d
Ragum02 <benjamin.souiket@gmail.com>
2025-08-28 21:44:10 +0200

    test: add unit tests for Navbar without handling Auth

--- tests/app/components/Navbar.test.tsx
+++ tests/app/components/Navbar.test.tsx
@@ -1,19 +1,43 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Navbar from '@/app/components/Navbar';
 
-describe('Navbar component', () => {
-  it('should render without crashing', () => {
+describe('Navbar structure', () => {
+  beforeEach(() => {
     render(<Navbar />);
   });
 
-  it('should contain link to Accueil', () => {
-    render(<Navbar />);
-    expect(screen.getByText('Accueil')).toBeInTheDocument();
+  it('renders the title link', () => {
+    const nav = screen.getByRole('navigation');
+    const titleLink = within(nav).getByRole('link', { name: 'CycloBuddy' });
+    expect(titleLink).toHaveAttribute('href', '/');
   });
 
-  it('should contain link to Sorties', () => {
-    render(<Navbar />);
-    expect(screen.getByText('Sorties')).toBeInTheDocument();
+  it('renders the navigation list with 3 links', () => {
+    const list = screen.getByRole('list');
+    const listItems = within(list).getAllByRole('listitem');
+    expect(listItems).toHaveLength(3);
+
+    expect(within(list).getByRole('link', { name: 'Sorties' })).toHaveAttribute(
+      'href',
+      '/rides',
+    );
+    expect(within(list).getByRole('link', { name: 'Groupes' })).toHaveAttribute(
+      'href',
+      '/groups',
+    );
+    expect(
+      within(list).getByRole('link', { name: 'Nouvelle sortie' }),
+    ).toHaveAttribute('href', '/create-ride');
+  });
+
+  it('renders the account actions', () => {
+    const actions = screen.getByRole('group', { name: /account actions/i });
+    expect(
+      within(actions).getByRole('link', { name: 'Login' }),
+    ).toHaveAttribute('href', '/login');
+    expect(
+      within(actions).getByRole('link', { name: 'S’inscrire' }),
+    ).toHaveAttribute('href', '/register');
   });
 });
