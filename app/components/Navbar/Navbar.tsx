341ccf376f53dc0da998031c9f15ceac2d9c0a53
Ragum02 <benjamin.souiket@gmail.com>
2025-08-29 09:06:37 +0200

    style: add hover on the app title in the navbar

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -10,8 +10,10 @@ export default function Navbar() {
 
   return (
     <nav className="text-white fixed top-0 z-20 w-screen flex justify-between items-center bg-[rgba(12,30,62,0.95)] px-4 py-6 backdrop-blur-md">
-      <Link href="/" className="text-2xl font-bold">
-        CycloBuddy
+      <Link href="/">
+        <span className="text-2xl font-bold hover:text-shadow-[0_0_5px_rgba(255,255,255,0.7)]">
+          CycloBuddy
+        </span>
       </Link>
 
       {isMobile ? <BurgerMenu /> : <NavigationsLinks />}
