5deda0a9bb8aebf56a21f5718f3f84b571f33bbc
Ragum02 <benjamin.souiket@gmail.com>
2025-08-29 10:51:29 +0200

    style: add gradient to burger menu

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,10 +1,12 @@
 export default function Footer() {
   return (
-    <footer>
-      <nav></nav>
-      <nav></nav>
-      <nav></nav>
-      <nav></nav>
+    <footer className="flex absolute bottom-0 w-full flex-row justify-between items-center bg-red-800">
+      <nav aria-label="footer main title and mobile application link">
+        hello
+      </nav>
+      <nav aria-label="footer navigations links">there</nav>
+      <nav aria-label="footer support links">test</nav>
+      <nav aria-label="footer legal links">test</nav>
     </footer>
   );
 }
